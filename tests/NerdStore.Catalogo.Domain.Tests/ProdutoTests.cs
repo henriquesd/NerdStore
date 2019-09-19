f79280ab71d551c95722820cdff42bdb9f27cbc9
henriquesd <hsd.foz@gmail.com>
2019-09-19 17:54:40 -0300

    Testando comportamentos

---
+++ tests/NerdStore.Catalogo.Domain.Tests/ProdutoTests.cs
@@ -0,0 +1,53 @@
+using NerdStore.Core.DomainObjects;
+using System;
+using Xunit;
+
+namespace NerdStore.Catalogo.Domain.Tests
+{
+    public class ProdutoTests
+    {
+        [Fact]
+        public void Produto_Validar_ValidacoesDevemRetornarExceptions()
+        {
+            // Arrange & Act & Assert
+
+            var ex = Assert.Throws<DomainException>(() =>
+                new Produto(string.Empty, "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Nome do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", string.Empty, false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Descricao do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", "Descricao", false, 0, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Valor do produto não pode se menor igual a 0", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", "Descricao", false, 100, Guid.Empty, DateTime.Now, "Imagem", new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo CategoriaId do produto não pode estar vazio", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() =>
+                new Produto("Nome", "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, string.Empty, new Dimensoes(1, 1, 1))
+            );
+
+            Assert.Equal("O campo Imagem do produto não pode estar vazio", ex.Message);
+
+            // o certo seria criar um teste de unidade para testar as Dimensoes, se ele está validando todos os parâmetros;
+            // o código aqui é só exemplo para mostrar a importância de validar;
+            ex = Assert.Throws<DomainException>(() =>
+              new Produto("Nome", "Descricao", false, 100, Guid.NewGuid(), DateTime.Now, "Imagem", new Dimensoes(0, 1, 1))
+            );
+
+            Assert.Equal("O campo Altura não pode ser menor ou igual a 0", ex.Message);
+        }
+    }
+}
