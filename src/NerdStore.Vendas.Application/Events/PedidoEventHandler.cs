7a18b0c6df8c775a188541ab7134b10dae081701
henriquesd <hsd.foz@gmail.com>
2019-09-30 17:55:29 -0300

    Integração de BC's - Processamento do pedido

--- src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
+++ src/NerdStore.Vendas.Application/Events/PedidoEventHandler.cs
@@ -2,14 +2,15 @@
 using System.Threading.Tasks;
 using MediatR;
 using NerdStore.Core.Communication.Mediator;
-
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
 
 namespace NerdStore.Vendas.Application.Events
 {
     public class PedidoEventHandler :
          INotificationHandler<PedidoRascunhoIniciadoEvent>,
          INotificationHandler<PedidoAtualizadoEvent>,
-         INotificationHandler<PedidoItemAdicionadoEvent>
+         INotificationHandler<PedidoItemAdicionadoEvent>,
+         INotificationHandler<PedidoEstoqueRejeitadoEvent>
     {
         public Task Handle(PedidoRascunhoIniciadoEvent notification, CancellationToken cancellationToken)
         {
@@ -23,6 +24,12 @@ namespace NerdStore.Vendas.Application.Events
 
         public Task Handle(PedidoItemAdicionadoEvent notification, CancellationToken cancellationToken)
         {
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(PedidoEstoqueRejeitadoEvent message, CancellationToken cancellationToken)
+        {
+            // cancelar o processamento do pedido - retornar erro para o cliente;
             return Task.CompletedTask;
         }
     }
